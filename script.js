ee185d60c2f2a8a20f8fc3fc92cef2084d0972f8
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-10-22 10:42:47 -0700

    ABOUT PAGE COMPLETE

---
+++ script.js
@@ -0,0 +1,55 @@
+
+var likebtn = document.getElementById("like");
+var i = document.getElementById("i");
+likebtn.addEventListener("click",()=>{
+    i.style.color="red"
+    likebtn.style.display="none"
+})
+var read = document.getElementById("readmore");
+read.addEventListener("click",()=>{
+    var p = document.getElementById("rep").innerHTML="dolor sit amet consectetur, aspernatur officia odi";
+    read.style.display="none"
+
+})
+
+var likebtnn = document.getElementById("likee");
+var ii = document.getElementById("ii");
+likebtnn.addEventListener("click",()=>{
+    ii.style.color="red"
+    likebtnn.style.display="none"
+})
+var readd = document.getElementById("readmoree");
+readd.addEventListener("click",()=>{
+    var pp = document.getElementById("repp").innerHTML="dolor sit amet consectetur, aspernatur officia odi";
+    readd.style.display="none"
+
+})
+
+
+var likebtnnn = document.getElementById("likeee");
+var iii = document.getElementById("iii");
+likebtnnn.addEventListener("click",()=>{
+    iii.style.color="red"
+    likebtnnn.style.display="none"
+})
+var readdd = document.getElementById("readmoreee");
+readdd.addEventListener("click",()=>{
+    var ppp = document.getElementById("reppp").innerHTML="dolor sit amet consectetur, aspernatur officia odi";
+    readdd.style.display="none"
+
+})
+
+
+
+var likebtnnnn = document.getElementById("likeeee");
+var iiii = document.getElementById("iiii");
+likebtnnnn.addEventListener("click",()=>{
+    iiii.style.color="red"
+    likebtnnnn.style.display="none"
+})
+var readddd = document.getElementById("readmoreeee");
+readddd.addEventListener("click",()=>{
+    var pppp = document.getElementById("repppp").innerHTML="dolor sit amet consectetur, aspernatur officia odi";
+    readddd.style.display="none"
+
+})
